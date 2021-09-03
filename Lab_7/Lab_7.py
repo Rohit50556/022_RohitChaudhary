205a7ea550358ee2850144b9987233feb0a47e61
Rohit50556 <49819885+Rohit50556@users.noreply.github.com>
2021-09-03 15:40:00 +0530

    final commit

--- Lab_7/Lab_7.py
+++
@@ -1,9 +0,0 @@
-
-import nltk
-from nltk.corpus import twitter_samples 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-import tensorflow.compat.v1 as tf
-
-nltk.download('twitter_samples')
-nltk.download('stopwords')
